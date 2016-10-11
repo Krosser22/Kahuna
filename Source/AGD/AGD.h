2b31ab6e83e8d48fddf9c04e95577974d1e8b0dc
David Parra <davidparraausina@gmail.com>
2016-10-11 13:15:27 +0100

    Base UE 4.13.1 Project

---
+++ Source/AGD/AGD.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __AGD_H__
+#define __AGD_H__
+
+#include "EngineMinimal.h"
+
+#endif
---
+++ Source/AGD/AGDGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "AGD.h"
+#include "AGDGameMode.h"
+#include "AGDCharacter.h"
+
+AAGDGameMode::AAGDGameMode()
+{
+	/*
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+	*/
+}
