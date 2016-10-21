1eb99a7626311c205b1666b8534951affe142fd0
David Parra <davidparraausina@gmail.com>
2016-10-21 23:52:56 +0100

    delete

--- Reload.bat
+++
@@ -1,6 +0,0 @@
-rd /s /q .vs
-rd /s /q Binaries
-rd /s /q Intermediate
-rd /s /q Saved
-del "*.sln"
-del "*.db"
--- Source/TheGallery/TheGallery.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#include "TheGallery.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheGallery, "TheGallery" );
- 
