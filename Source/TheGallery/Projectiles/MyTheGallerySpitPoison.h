e015e6270d25b5a90c6e367ad93ecb1886d05044
Daniel Laserna Condado <condadodaniel@hotmail.com>
2016-11-08 21:51:14 +0000

    Added a new projectile to the spit poison of the enemies. Updated the AI map.

---
+++ Source/TheGallery/Projectiles/MyTheGallerySpitPoison.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Projectiles/TheGalleryProjectile.h"
+#include "MyTheGallerySpitPoison.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGALLERY_API AMyTheGallerySpitPoison : public ATheGalleryProjectile
+{
+	GENERATED_BODY()
+};
